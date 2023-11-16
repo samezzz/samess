0da163b00fe759efe998da146b8b08103050bdfa
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2023-11-16 06:18:29 +0000

    fixed icon

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,13 @@
+import { Icons } from "@/components/icons"
+
 export type NavItem = {
     name: string
     href: string
     disabled?: boolean
+}
+
+export type LinkItem = {
+    name: string
+    href: string
+    icon?: keyof typeof Icons
 }
